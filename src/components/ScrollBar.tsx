2fe91fd936487e2480937c8510f2a210a7ca7100
soohyeon <su132489@gmail.com>
2023-04-27 01:41:13 +0900

    Refactor: 스크롤바 리팩토링

--- src/components/ScrollBar.tsx
+++ src/components/ScrollBar.tsx
@@ -9,86 +9,75 @@ type ScrollBarProps = {
 }
 
 const ScrollBar = ({ wrapperWidth, list, listWidth, listScrollWidth }:ScrollBarProps) => {
-    const scrollbar: React.MutableRefObject<HTMLDivElement | null> = useRef(null); // 스크롤바 DOM
-    const mouseDownX = useRef<number>(0); // 시작 마우스 X값
-    const currentMouseX = useRef<number>(0); // 지금 마우스 X값
-    const prevScrollbarX = useRef<number>(0); // 이전 스크롤바 위치 X값
-    const [scrollbarMoveX, setScrollbarMoveX] = useState<number>(0); // 계산된 스크롤바 위치
-    const [scrollbarMaxX, setScrollbarMaxX] = useState<number>(300); // 스크롤바가 이동할 수 있는 최대 위치
-    const [isMouseDowned, setIsMouseDowned] = useState<boolean>(false);
-    const [isthrottle, setIsThrottle] = useState<boolean>(false);
-    const isAddedEvent = useRef<boolean>(false); // window에 mousemove 이벤트를 여러개 생성하지 않기 위해 이벤트 생성 여부 저장
-    const [scrollbarWidth, setScrollbarWidth] = useState<number>(0);
+    const scrolltrack: React.MutableRefObject<HTMLDivElement | null> = useRef(null);
+    const scrollbar: React.MutableRefObject<HTMLDivElement | null> = useRef(null);
+    const mouseDownX = useRef<number>(0); // 시작 마우스 X위치(마우스가 다운 된 순간)
+    const prevScrollbarX = useRef<number>(0); // 이전 스크롤바 X위치
+    const scrollbarMoveX = useRef<number>(0); // 스크롤바가 이동할 계산된 X위치
+    const scrollbarMaxX = useRef<number>(300); // 스크롤바가 이동할 수 있는 최대 X위치
+    const isthrottle = useRef<boolean>(false);
+    const scrollbarWidth = useRef<number>(0);
     const [ishiddenScrollbar, setIshiddenScrollbar] = useState<boolean>(true); // 아직 스크롤 콘텐츠의 데이터가 없을 때 스크롤바를 숨기기 위함
 
-    useEffect(() => { // 스크롤바가 이동할 수 있는 최대 위치 업데이트함
-        if (wrapperWidth && wrapperWidth - scrollbarWidth !== 0) {
-            setScrollbarMaxX(wrapperWidth - scrollbarWidth)
-        }
-    },[wrapperWidth, scrollbarWidth]);
-
     useEffect(() => { // 스크롤바 길이를 업데이트함
-        if (listWidth && listScrollWidth && scrollbar.current) { 
+        if (wrapperWidth && listWidth && listScrollWidth && scrolltrack.current && scrollbar.current) { 
             if (listWidth === listScrollWidth) {
-                setScrollbarWidth(0);
+                scrollbarWidth.current = 0;
             } else {
                 setIshiddenScrollbar(false);
-                setScrollbarWidth((listWidth / listScrollWidth) * scrollbar.current.clientWidth);
+                scrollbarWidth.current = (listWidth / listScrollWidth) * scrolltrack.current.clientWidth;
             }
+                scrollbar.current.style.setProperty("width", `${scrollbarWidth.current}px`); // 스크롤바 길이 업데이트
+                scrollbarMaxX.current = wrapperWidth - scrollbarWidth.current; // 스크롤바가 이동할 수 있는 최대 위치 업데이트함
         }
-    },[listWidth, listScrollWidth]);
+    },[listWidth, listScrollWidth, scrollbar, scrollbarWidth, wrapperWidth, scrollbarMaxX]);
 
-    const onMouseMove = (e: MouseEvent<HTMLDivElement, globalThis.MouseEvent>) => {
-        if (isMouseDowned) {
-            if (isthrottle) {
-                // console.log('스로틀!');
-            } else if (!isthrottle) {
-                setIsThrottle(true);
-                setTimeout(async() => {
-                    let newScrollbarMoveX = prevScrollbarX.current + (currentMouseX.current - mouseDownX.current);
-                    // console.log("시작 마우스 X값 : ", mouseDownX.current, "\n지금 마우스 X값 : ", currentMouseX.current, 
-                    //     "\n이전 위치 : ", prevScrollbarX.current, "\n새 위치(지금X - 시작X) : ", newScrollbarMoveX);
-                    if (newScrollbarMoveX < 0) { // 스크롤 영역을 넘어가지 않도록함
-                        newScrollbarMoveX = 0;
-                    } else if (newScrollbarMoveX > scrollbarMaxX) {
-                        newScrollbarMoveX = scrollbarMaxX;
-                    }
-                    setScrollbarMoveX(newScrollbarMoveX);
-                    if (list && listWidth && listScrollWidth && scrollbar.current) { // 실제 스크롤에 반영
-                        const moveLeft = (newScrollbarMoveX / listWidth) * listScrollWidth;
-                        list.scrollLeft = moveLeft;
-                        // console.log('실제 list가 움직일 거리 : ' , moveLeft, ' = (', newScrollbarMoveX, " / ", listWidth, ") *", listScrollWidth);
-                    }
-                    setIsThrottle(false);
-                }, 80);
-            }
+    const updateScrollX = useCallback((e: globalThis.MouseEvent) => {
+        if (!isthrottle.current) {
+            isthrottle.current = true;
+            setTimeout(async() => {
+                let newScrollbarMoveX = prevScrollbarX.current + (e.pageX - mouseDownX.current);
+                // console.log("이전 위치 : ", prevScrollbarX.current, 
+                //     "\n새 위치(지금X - 시작X) : ", newScrollbarMoveX, " = ", e.pageX, " - ", mouseDownX.current);
+                if (newScrollbarMoveX < 0) { // 스크롤바가 스크롤트랙 영역을 넘어가지 않도록함
+                    newScrollbarMoveX = 0;
+                } else if (newScrollbarMoveX > scrollbarMaxX.current ) {
+                    newScrollbarMoveX = scrollbarMaxX.current ;
+                }
+                if (scrollbar.current) { // 스크롤바 옮기기
+                    scrollbar.current.style.setProperty("transform", `translateX(${newScrollbarMoveX}px)`);
+                }
+                if (list && listWidth && listScrollWidth && scrolltrack.current) { // 실제 리스트를 스크롤하기
+                    const moveLeft = (newScrollbarMoveX / listWidth) * listScrollWidth;
+                    list.scrollLeft = moveLeft;
+                    // console.log('실제 list가 움직일 거리 : ' , moveLeft, ' = (', newScrollbarMoveX, " / ", listWidth, ") *", listScrollWidth);
+                }
+                scrollbarMoveX.current = newScrollbarMoveX;
+                isthrottle.current = false;
+            }, 80);
         }
-    };
-    const onMouseDown = (e: MouseEvent<HTMLDivElement, globalThis.MouseEvent>) => {
-        // console.log('다운!', e.pageX);
-        setIsMouseDowned(true);
-        mouseDownX.current = e.pageX;
-    }
-    const UpdatecurrentMouseX = useCallback((e: globalThis.MouseEvent) => {
-        currentMouseX.current = e.pageX;
-    }, []);
-    const onMouseUp = () => {
-        // console.log('업!');
-        setIsMouseDowned(false);
-        prevScrollbarX.current = scrollbarMoveX;
-    }
+    },[list, listScrollWidth, listWidth]);
+
+    const onMouseUp = useCallback(() => {
+        window.removeEventListener('mouseup', onMouseUp);
+        window.removeEventListener('mousemove', updateScrollX);
+        prevScrollbarX.current = scrollbarMoveX.current;
+    },[updateScrollX]);
+    const onMouseDown = useCallback((e: MouseEvent<HTMLDivElement, globalThis.MouseEvent>) => {
+        if (e.target === scrollbar.current) {
+            window.addEventListener('mouseup', onMouseUp);
+            window.addEventListener('mousemove', updateScrollX);
+            mouseDownX.current = e.pageX;
+        }
+    }, [onMouseUp, updateScrollX]);
+
     // 스크롤바에 onMouseDown이 발생하면, 처음 Down이 발생한 X위치와 현재 마우스의 X위치의 차이를 계산하여
-    // 스크롤을 움직입니다. 마우스가 스크롤트랙을 벗어나거나 onMouseUp이 발생하면 멈춥니다.
+    // 스크롤을 움직입니다. onMouseUp이 발생하면 멈춥니다.
     return (
-        <ScrollBarWrapper id="forecast-scroll" className="scroll-track" ref={scrollbar}
-            onMouseMove={(e) => {onMouseMove(e); if (!isMouseDowned && !isAddedEvent.current) { window.addEventListener('mousemove', UpdatecurrentMouseX); isAddedEvent.current = true;}}}
-            onMouseLeave={() => { window.removeEventListener('mousemove', UpdatecurrentMouseX); isAddedEvent.current = false; onMouseUp();}}
-            onMouseUp={() => {onMouseUp();}}
+        <ScrollBarWrapper id="forecast-scroll" className="scroll-track" ref={scrolltrack}
+            onMouseDown={(e) => {onMouseDown(e);}}
         >
-            <div className={ishiddenScrollbar ? "scroll-bar hidden" : "scroll-bar"} 
-                style={{width: `${scrollbarWidth}px`, transform: `translateX(${scrollbarMoveX}px)`}}
-                onMouseDown={(e) => {onMouseDown(e);}}
-            >
+            <div className={ishiddenScrollbar ? "scroll-bar hidden" : "scroll-bar"} ref={scrollbar} >
                 <img className="reverse" src={`${process.env.PUBLIC_URL}/image/icon/line-arrow.svg`} alt="left-arrow-icon" />
                 <img src={`${process.env.PUBLIC_URL}/image/icon/line-arrow.svg`} alt="right-arrow-icon" />
             </div>
